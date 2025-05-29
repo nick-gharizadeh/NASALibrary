581eac1970bdbe763412b44dc1edec8b32e7bf13
Nick <nickgharizadeh@gmail.com>
2025-05-29 16:23:26 +0330

    imp / placeholder&error for adapter images

--- app/src/main/java/com/project/nasalibrary/adapter/PopularAdapter.kt
+++ app/src/main/java/com/project/nasalibrary/adapter/PopularAdapter.kt
@@ -1,6 +1,7 @@
 package com.project.nasalibrary.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
@@ -8,9 +9,11 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.project.nasalibrary.R
 import com.project.nasalibrary.databinding.MainItemBinding
 import com.project.nasalibrary.model.Item
 import com.project.nasalibrary.utils.BaseDiffUtils
+import com.project.nasalibrary.utils.Constants
 import javax.inject.Inject
 
 
@@ -21,7 +24,7 @@ class PopularAdapter @Inject constructor() : RecyclerView.Adapter<PopularAdapter
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         binding = MainItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder()
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(items[position])
@@ -32,26 +35,31 @@ class PopularAdapter @Inject constructor() : RecyclerView.Adapter<PopularAdapter
 
     override fun getItemId(position: Int) = position.toLong()
 
-    inner class ViewHolder : RecyclerView.ViewHolder(binding.root) {
-        fun bind(item: Item) {
-            binding.apply {
-                //Text
-                textViewTitle.text = item.data[0].title ?: ""
-                //Image
-                val imageHref = item.links?.get(0)?.href
-                Glide.with(itemView)
-                    .load(imageHref)
-                    .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
-                    .transform(CenterCrop(), RoundedCorners(20))
-                    .into(binding.imageViewMainItem)
-                //Click
-                root.setOnClickListener {
-                    onItemClickListener?.let {
-                        it(item)
+    inner class ViewHolder(private val binding: MainItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Item?) {
+            item?.let { currentItem ->
+                binding.apply {
+                    val firstDataItem = currentItem.data.firstOrNull()
+                    imageViewPlayIcon.visibility =
+                        if (firstDataItem?.mediaType == Constants.VIDEO_MEDIA_TYPE) View.VISIBLE else View.GONE
+                    textViewTitle.text = firstDataItem?.title ?: ""
+
+                    val imageHref = currentItem.links?.firstOrNull { it?.href != null }?.href
+                    Glide.with(itemView.context)
+                        .load(imageHref)
+                        .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+                        .transform(CenterCrop(), RoundedCorners(20))
+                        .placeholder(R.drawable.placeholder_image)
+                        .error(R.drawable.ic_error_image)
+                        .into(imageViewMainItem)
+
+
+                    root.setOnClickListener {
+                        onItemClickListener?.invoke(currentItem)
                     }
                 }
             }
-
         }
     }
 
